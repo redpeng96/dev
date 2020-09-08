57c009632b6daee282306778270597c23e98dcc9
Joseph Park <jpark@Josephs-MacBook-Air.local>
2020-09-08 09:31:50 -0400

    commit

--- WebCrawler/3_requests.py
+++ WebCrawler/3_requests.py
@@ -1,5 +1,12 @@
 import requests
 
-res = requests.get("https://www.naver.com")
 
-print(res.status_code)
+#res = requests.get("https://www.naver.com")
+res = requests.get("https://www.google.com")
+
+res.raise_for_status()
+
+print(len(res.text))
+
+with open("google.html", "w", encoding="utf8") as f:
+    f.write(res.text)
