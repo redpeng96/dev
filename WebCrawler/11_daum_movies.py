da930793273c04f6fe6be48ff327f17b89342376
Joseph Park <jpark@Josephs-MacBook-Air.local>
2020-09-09 13:32:58 -0400

    commit

---
+++ WebCrawler/11_daum_movies.py
@@ -0,0 +1,29 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"}
+#for i in range(9, 4, -1):
+i = 9
+url = "https://search.daum.net/search?w=tot&q=201{}%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR".format(i)
+#print(url)
+
+res = requests.get(url, headers=header)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+images = soup.find_all("img", attrs={"class":"thumb_img"})
+
+for idx, image in enumerate(images):
+    img_url = image["src"]
+    if img_url.startswith("//"):
+        img_url = "https:" + img_url
+        print(img_url)
+        img_res = requests.get(img_url)
+        img_res.raise_for_status()
+
+        with open("movie_images{}.jpg".format(idx+1), "wb") as f:
+            f.write(img_res.content)
+
+
+    else:
+        continue
