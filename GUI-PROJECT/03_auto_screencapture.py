9ef044c12c189b093ba15d26d0f82e9151437304
Joseph Park <jpark@Josephs-MacBook-Air.local>
2020-09-18 10:46:32 -0400

    ...

--- GUI-PROJECT/03_auto_screencapture.py
+++ GUI-PROJECT/03_auto_screencapture.py
@@ -1,1 +1,10 @@
-from PIL 
+import time
+from PIL import ImageGrab
+
+time.sleep(5)
+
+for i in range(1, 11):
+    img = ImageGrab.grab()
+    img.save("image{}.png".format(i))
+    time.sleep(2)
+    print(f"Image{i} is captured...")
